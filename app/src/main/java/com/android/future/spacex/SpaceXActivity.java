a4deeb258f8881ccef87bc854ea1fd41d8d0000c
Ciprian Bindiu <ciprian.bindiu@gmail.com>
2018-06-28 15:59:53 +0300

    Create design template for SpaceX activity

--- app/src/main/java/com/android/future/spacex/SpaceXActivity.java
+++ app/src/main/java/com/android/future/spacex/SpaceXActivity.java
@@ -1,7 +1,9 @@
 package com.android.future.spacex;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SpaceXActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class SpaceXActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_space_x);
+
+//        Typeface brandonBlack = Typeface.createFromAsset(getAssets(), "Brandon_blk.otf");
+//        TextView missionNameTextView = findViewById(R.id.mission_name);
+//        TextView missionNameTextView2 = findViewById(R.id.mission_name2);
+//        TextView missionNameTextView22 = findViewById(R.id.mission_name22);
+//        missionNameTextView.setTypeface(brandonBlack);
+//        missionNameTextView2.setTypeface(brandonBlack);
+//        missionNameTextView22.setTypeface(brandonBlack);
     }
 }
