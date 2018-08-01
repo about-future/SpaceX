e0680d4835dac3f5312da2be9b9813a0db74511c
Ciprian Bindiu <ciprian.bindiu@gmail.com>
2018-08-02 01:37:40 +0300

    Clean

--- app/src/main/java/com/about/future/spacex/SettingsFragment.java
+++ app/src/main/java/com/about/future/spacex/SettingsFragment.java
@@ -1,9 +1,7 @@
 package com.about.future.spacex;
 
-import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.v14.preference.SwitchPreference;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
